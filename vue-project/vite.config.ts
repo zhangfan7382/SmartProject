38f97e6747963b8bc80920e8dedc639ddb8a7a05
zhangfan7382 <15093525720@163.com>
2025-05-19 16:10:27 +0800

    1

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/SmartProject/',
+  // base:'/SmartProject/',
   plugins: [
     vue(),
     vueJsx(),
