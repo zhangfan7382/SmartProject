2f8c310d05fc06aa990c0fc8ccef404f6ba8ff31
zhangfan7382 <15093525720@163.com>
2025-05-19 16:58:59 +0800

    修改路由模式，以及 出现 404 的处理方式

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SimulationView from '../views/SimulationView.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
